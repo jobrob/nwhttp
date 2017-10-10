36f1fee38f27fb124d723b5fc7a04be55d7aed92
James Cope <ashwincope@gmail.com>
2017-10-10 23:46:31 +0100

    Implemented basic GET request handling; Eclipse .classpath and .settings files modified to handle bug relating to not compiling source correctly when using Files.get(String) method

--- src/Server.java
+++ src/Server.java
@@ -1,5 +1,6 @@
 import java.io.*;
 import java.net.*;
+import java.nio.file.*;
 
 /**
  * This is the main web server
@@ -9,41 +10,57 @@ public class Server {
 	public static void main(String[] args) throws Exception {
 		final int PORT = 4000;
 		
-		System.err.print("Opening socket on port "+PORT+" ... ");
+		System.err.print("Opening master socket on port "+PORT+" ... ");
 		ServerSocket ss = new ServerSocket(PORT);
-		System.err.println("done.");
+		System.err.println("done.\n");
 		
 		while (true) {
 			System.err.print("Listening for connection on port "+PORT+" ... ");
-			Socket cxn = ss.accept();
-			System.err.println("made connection with "+cxn.getInetAddress());
+			Socket client = ss.accept();
+			System.err.println("made connection with "+client.getInetAddress());
 			
-			System.err.print("Creating input stream ... ");
-			BufferedReader in = new BufferedReader(new InputStreamReader(cxn.getInputStream()));
-			System.err.println("done.");
-			
-			System.err.print("Creating output stream ... ");
-			PrintStream out = new PrintStream(cxn.getOutputStream());
-			System.err.println("done.");
-			
-			System.err.println("INPUT RECEIVED:");
-			String input;
-			while((input = in.readLine()) != null && !(input.equals(""))) {
-				System.err.println("OK: "+input);
-			}
-			
-			System.err.println("POSTING OUTPUT...");
-			out.print(
-				  "<!doctype html>"
-			+	"\n<html>"
-			+	"\n<head><title>A webpage</title></head>"
-			+	"\n<body><h1>A heading</h1><p>Some paragraph text</p></body>"
-			+	"\n</html>"
-			);
+			handleHTTPRequest(client);
 			
 			System.err.print("Terminating connection ... ");
-			cxn.close();
-			System.err.println("done.");
+			client.close();
+			System.err.println("done.\n");
+		}
+	}
+	
+	public static void handleHTTPRequest(Socket client) throws Exception {
+		System.err.print("Creating input stream ... ");
+		BufferedReader in = new BufferedReader(new InputStreamReader(client.getInputStream()));
+		System.err.println("done.");
+		
+		System.err.print("Creating output stream ... ");
+		PrintStream out = new PrintStream(client.getOutputStream());
+		System.err.println("done.");
+		
+		String[] request = in.readLine().split(" ");
+		switch (request[0]) {
+			case "OPTIONS":
+				break;
+			case "GET":
+				try {
+					Files.copy(Paths.get("./site"+request[1]), out);
+				} catch (Exception e) {
+					System.err.println("ERROR: Could not process GET request: "+request[1]);
+				}
+				break;
+			case "HEAD":
+				break;
+			case "POST":
+				break;
+			case "PUT":
+				break;
+			case "DELETE":
+				break;
+			case "TRACE":
+				break;
+			case "CONNECT":
+				break;
+			default:
+				break;
 		}
 	}
 }
